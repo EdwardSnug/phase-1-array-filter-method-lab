cc56b50ee78f82b65522d6454ec124478d6bdc98
Edward Karogo <prezokams@gmail.com>
2025-06-18 17:38:10 +0300

    Filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(drivers, stringvar){
+    return drivers.filter(function(driver){
+        return driver.toLowerCase() === stringvar.toLowerCase();
+    });
+}
+function fuzzyMatch(drivers, stringVar) {
+  return drivers.filter(function(driver) {
+    // Convert both to lowercase and compare the string variables
+    for (let i = 0; i < stringVar.length; i++) {
+      if (driver[i].toLowerCase() !== stringVar[i].toLowerCase()) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+function matchName(drivers, nameToMatch) {
+  return drivers.filter(function(driver) {
+    return driver.name === nameToMatch;
+  });
+}
+
